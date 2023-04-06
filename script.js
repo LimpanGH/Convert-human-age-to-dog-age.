82593f02f116a3baacfcd65eff0d70b4b28a5fe4
LimpanGH <108390176+LimpanGH@users.noreply.github.com>
2023-04-06 10:42:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let myAge = 40;
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Doktor Trubbel'.toLowerCase();
+
+console.log(earlyYears);
+console.log(laterYears);
+console.log(myAgeInDogYears);
+console.log(`My name is ${myName}. I am ${myAge} years which is ${myAgeInDogYears} years in dog years.`)
